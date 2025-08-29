0a566ff525fe86345d4b521cd6d4dca24e67f462
Devawayan <devawayan.hore-extern@renault.com>
2025-08-29 22:03:13 +0530

    Conversations

--- apps/widget/modules/widget/atoms/widget-atoms.ts
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -4,14 +4,12 @@ import { atomFamily, atomWithStorage } from "jotai/utils";
 import { CONTACT_SESSION_KEY } from "../constants";
 import { Id } from "@workspace/backend/_generated/dataModel";
 
-//Basic widget Atoms
 export const screenAtom = atom<WidgetScreen>("loading");
 export const organizationIdAtom = atom<string | null>(null);
 
-//Organization-scoped contact session Atom
 export const contactSessionIdAtomFamily = atomFamily((organizationId: string) => {
     return atomWithStorage<Id<"contactSessions"> | null>(`${CONTACT_SESSION_KEY}_${organizationId}`, null) 
 });
-
 export const errorMessageAtom = atom<string | null>(null);
 export const loadingMessageAtom = atom<string | null>(null);
+export const conversationIdAtom = atom<Id<"conversations"> | null>(null);
